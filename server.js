34807821e0b3995719a49389ca24589a29b5612c
Ritika Gupta <ritika.emilence@gmail.com>
2018-06-14 17:27:46 +0530

    Initialize repo

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+// app.get('/',function (req, res)
+// {
+// res.send('Hello Express!');
+
+// });
+var middleware = {
+	requireAuthentication:function(req,res,next)
+	{
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req,res,next)
+	{
+		console.log(new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication,function (req, res)
+{
+res.send('About us!');
+
+});
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000,function()
+{
+	console.log('Express server started!');
+	});
